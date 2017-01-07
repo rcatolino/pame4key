c7cd3ea5416077c78a55d0364ad1c0a44681d035
Raphael Catolino <raphael.catolino@gmail.com>
2017-01-07 23:05:55 +0100

    Add key derivation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-extern crate pam_sm;
 #[macro_use] extern crate nix;
+extern crate pam_sm;
+extern crate ring;
 
 use pam_sm::pam::{PamServiceModule, Pam, PamFlag, PamReturnCode};
+use ring::{digest, pbkdf2};
 use std::error::Error;
 use std::ffi::CStr;
 use std::fmt;
@@ -9,6 +11,8 @@ use std::fs::File;
 use std::process::{Command, Stdio};
 use std::io::Write;
 use std::os::unix::io::AsRawFd;
+
+static PBKDF2_ITERATIONS: usize = 0xFFFF;
 
 struct SM;
 
@@ -27,6 +31,39 @@ impl Salt {
 impl fmt::Display for Salt {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for b in self.0.iter() {
+            try!(write!(f, "{:02X}", b));
+        }
+        Ok(())
+    }
+}
+
+struct Ext4Key {
+    key: [u8; 64],
+    key_ref: [u8; 8],
+}
+
+impl Ext4Key {
+    fn new(token: &[u8], salt: Salt) -> Ext4Key {
+        let mut key = [0u8; 64];
+        println!("Using token: {:?}", token);
+        println!("Using salt : {}", salt);
+        pbkdf2::derive(&pbkdf2::HMAC_SHA512, PBKDF2_ITERATIONS, &salt.0, token, &mut key);
+        let digest = digest::digest(&digest::SHA512, digest::digest(&digest::SHA512, &key).as_ref());
+        let mut key_ref = [0u8; 8];
+        for (src, dst) in digest.as_ref().iter().zip(key_ref.iter_mut()) {
+            *dst = *src;
+        }
+        Ext4Key { key: key, key_ref: key_ref }
+    }
+}
+
+impl fmt::Display for Ext4Key {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for b in self.key.iter() {
+            try!(write!(f, "{:02X}", b));
+        }
+        try!(write!(f, "::"));
+        for b in self.key_ref.iter() {
             try!(write!(f, "{:02X}", b));
         }
         Ok(())
@@ -60,12 +97,6 @@ impl PamServiceModule for SM {
         println!("In pam open_session !");
         println!("Getting auth token");
         println!("Args : {:?}", args);
-        for arg in args.iter() {
-            match get_salt(arg) {
-                Err(e) => println!("Error getting salt : {}", e),
-                Ok(salt) => println!("Got salt {}", salt),
-            }
-        }
         match pamh.get_authtok() {
             Err(e) => {
                 println!("Error getting password : {}", e);
@@ -77,6 +108,15 @@ impl PamServiceModule for SM {
             }
             Ok(Some(token)) => {
                 println!("Got token !");
+                for arg in args.iter() {
+                    match get_salt(arg) {
+                        Err(e) => println!("Error getting salt : {}", e),
+                        Ok(salt) => {
+                            println!("Got salt {}", salt);
+                            println!("Got key {}", Ext4Key::new(token.to_bytes(), salt));
+                        }
+                    }
+                }
                 if let Err(e) = add_key(token) {
                     println!("Error adding key : {}", e);
                     PamReturnCode::SYSTEM_ERR
@@ -91,4 +131,12 @@ impl PamServiceModule for SM {
 #[no_mangle]
 pub extern "C" fn get_pam_sm() -> Box<PamServiceModule> {
     return Box::new(SM {});
+}
+
+// This is used in some of rings tests and for some reason needs to be defined or pam fails to dlopen
+// pam_e4crypt.so
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern "C" fn RAND_bytes(_: *mut u8, _: *mut u8, _: usize) -> i32 {
+    0
 }
