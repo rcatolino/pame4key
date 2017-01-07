4682f3b4e4eaad7dbc5e6fa8d0992ef28f7ec27d
Raphael Catolino <raphael.catolino@gmail.com>
2017-01-07 16:25:58 +0100

    Add ioctl call to get salt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,37 @@
 extern crate pam_sm;
+#[macro_use] extern crate nix;
 
-use pam_sm::pam::{PamServiceModule, Pam, PamFlag, PamReturnCode, size_t};
+use pam_sm::pam::{PamServiceModule, Pam, PamFlag, PamReturnCode};
 use std::error::Error;
 use std::ffi::CStr;
+use std::fmt;
+use std::fs::File;
 use std::process::{Command, Stdio};
 use std::io::Write;
+use std::os::unix::io::AsRawFd;
 
 struct SM;
+
+struct Salt([u8; 16]);
+
+impl Salt {
+    fn new() -> Self {
+        Salt([0u8; 16])
+    }
+
+    fn as_ptr(&self) -> *const u8 {
+        self.0.as_ptr()
+    }
+}
+
+impl fmt::Display for Salt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for b in self.0.iter() {
+            try!(write!(f, "{:02X}", b));
+        }
+        Ok(())
+    }
+}
 
 fn add_key(token: &CStr) -> Result<(), Box<Error>> {
     let mut process = try!(Command::new("/usr/bin/e4crypt").stdin(Stdio::piped()).arg("add_key").arg("-q").spawn());
@@ -18,10 +43,29 @@ fn add_key(token: &CStr) -> Result<(), Box<Error>> {
     result
 }
 
+ioctl!(write buf fs_get_pwsalt with b'f', 20 ; u8);
+
+fn get_salt(path: &String) -> Result<Salt, Box<Error>> {
+    let f = try!(File::open(path));
+    let salt = Salt::new();
+    println!("File descriptor of {} : {}", path, f.as_raw_fd());
+    try!(unsafe {
+        fs_get_pwsalt(f.as_raw_fd(), salt.as_ptr(), 16)
+    });
+    Ok(salt)
+}
+
 impl PamServiceModule for SM {
-    fn authenticate(self: &Self, pamh: Pam, _: PamFlag,
-                        _: size_t, _: *const *const u8) -> PamReturnCode {
+    fn authenticate(self: &Self, pamh: Pam, _: PamFlag, args: Vec<String>) -> PamReturnCode {
+        println!("In pam open_session !");
         println!("Getting auth token");
+        println!("Args : {:?}", args);
+        for arg in args.iter() {
+            match get_salt(arg) {
+                Err(e) => println!("Error getting salt : {}", e),
+                Ok(salt) => println!("Got salt {}", salt),
+            }
+        }
         match pamh.get_authtok() {
             Err(e) => {
                 println!("Error getting password : {}", e);
